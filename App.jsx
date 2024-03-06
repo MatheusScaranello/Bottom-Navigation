006091ce5f37f091a2d934a8882880f4d772f57c
MatheusScaranello <matheusscaranello.564@gmail.com>
2024-03-06 08:55:11 -0300

    Add Contact and Profile screens

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./src/screens/Home";
+import ContactScreen from "./src/screens/Contact";
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -11,7 +12,6 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
       </Tab.Navigator>
-
       <StatusBar style="auto" />
     </NavigationContainer>
   );
